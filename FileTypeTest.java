efae9c6a6fd54d4f941472de1d18c08c56753413
nova-07-07 <nova234607ns@gmail.com>
2025-04-04 04:09:57 -0700

    Create FileTypeTest.java

---
+++ FileTypeTest.java
@@ -0,0 +1,6 @@
+public class FileTypeTest {
+    public static void main(String[] args) {
+        System.out.println("This file type is JAVA");
+        System.out.println("Running in Test Execution GUI");
+    }
+}
